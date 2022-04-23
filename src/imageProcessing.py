9177af290ab3af9cd227e3b237b3c685c6b45521
Quorafied <79426523+Quorafied@users.noreply.github.com>
2022-04-23 12:52:32 +0100

    Added main functionality for imageProcessing

--- src/imageProcessing.py
+++ src/imageProcessing.py
@@ -6,7 +6,10 @@ import numpy as np
 from PIL import Image
 from PIL import ImageGrab
 from PIL import ImageOps
-from PIL import ImageGrab
+
+import mss
+
+
 
 # Create imageProcessing class with constructor and image processing variables.
 class imageProcessing():
@@ -17,8 +20,42 @@ class imageProcessing():
         self.image_canny = None
         self.image_contours = None
 
+    # Get the image from the screen.
+    def screenshot(self, left=0, top=0, width=None, height=None):
+        # Get the image from the screen.
+        with mss.mss() as sct:
+            # Get the size of the screen.
+            monitor = {"top": top, "left": left, "width": width, "height": height}
+            # Get the image from the screen and convert to the right format.
+            img = sct.grab(monitor)
+            img = np.array(img)
 
+            # Convert the image to grayscale.
+            img = cv.cvtColor(img, cv.COLOR_BGRA2GRAY)
+            return img
 
+    # Perform template matching on the image.
+    def Template_Match(self, image, template):
+        # Get the size of the template.
+        w, h = template.shape[::-1]
+
+        # Perform match template on the image.
+        res = cv.matchTemplate(image, template, cv.TM_CCOEFF_NORMED)
+        threshold = 0.8
+        loc = np.where(res >= threshold)
+
+        # Return the coordinates of the template.
+        return loc
+
+# Test the imageProcessing class.
+if __name__ == "__main__":
+    print("Testing imageProcessing class.")
+    # Create an instance of the imageProcessing class.
+    imageProcessing = imageProcessing()
+    # Get the image from the screen.
+    image = imageProcessing.screenshot()
+    # Perform template matching on the image.
+        #loc = imageProcessing.Template_Match(image, template)
 
 
     
